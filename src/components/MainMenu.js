626586b65c226d57b67e7cecacf7be14ce198f69
jr-lol <jr-lol@live.com>
2021-09-07 11:56:32 -0300

    initial commit

---
+++ src/components/MainMenu.js
@@ -0,0 +1,10 @@
+import { MainMenuContainer } from '../styles/MaiMenuContainer'
+
+export const MainMenu = () => (
+  <MainMenuContainer>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+    </ul>
+  </MainMenuContainer>
+)
---
+++ src/styles/MoviesContainer.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const MoviesContainer = styled.section`
+  padding: 15px;
+  background-color: whitesmoke;
+  border: 2px solid black;
+  border-radius: 10px;
+`
